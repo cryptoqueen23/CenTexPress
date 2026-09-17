0924d3488cd9ef7dd9f0aa0b9376e5a30527c983
Mari <123911714+cryptoqueen23@users.noreply.github.com>
2026-09-17 12:30:46 -0500

    Set production site URL to centexpress.xyz

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://centexpress.example',
+  site: 'https://centexpress.xyz',
   integrations: [sitemap()],
 });
